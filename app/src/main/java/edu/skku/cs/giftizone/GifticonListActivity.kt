f0446d1f2a93fd126d4df2bded6fddc7bedce0d1
JeongMin <ajhappy12@gamil.com>
2023-05-30 23:39:26 +0900

    feat: tag toggle 기능 추가

--- app/src/main/java/edu/skku/cs/giftizone/GifticonListActivity.kt
+++ app/src/main/java/edu/skku/cs/giftizone/GifticonListActivity.kt
@@ -3,17 +3,21 @@ package edu.skku.cs.giftizone
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class GifticonListActivity : AppCompatActivity() {
-    private val tagList = ArrayList<String>()
+//    private val tagList = ArrayList<String>()
+    private val tagList = arrayListOf("tag1", "tagtag2")
+    private val selectedTag = HashSet<String>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gifticon_list)
 
-        val tagAddBtn = findViewById<Button>(R.id.TagAddButton)
+        val tagAddBtn = findViewById<ImageButton>(R.id.TagAddButton)
         tagAddBtn.setOnClickListener {
             val tagModal = TagModal(this) { tag ->
                 tagList.add(tag)
@@ -24,6 +28,6 @@ class GifticonListActivity : AppCompatActivity() {
         val tagListView = findViewById<RecyclerView>(R.id.tagListView)
         val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         tagListView.layoutManager = layoutManager
-        tagListView.adapter = TagAdapter(tagList)
+        tagListView.adapter = TagAdapter(tagList, selectedTag)
     }
 }
--- app/src/main/java/edu/skku/cs/giftizone/TagAdapter.kt
+++ app/src/main/java/edu/skku/cs/giftizone/TagAdapter.kt
@@ -5,15 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
-class TagAdapter(private val tagList: ArrayList<String>) :
+class TagAdapter(private val tagList: ArrayList<String>, private val selectedTag: HashSet<String>) :
     RecyclerView.Adapter<TagAdapter.TagViewHolder>() {
 
     inner class TagViewHolder(tagItemView: View) : RecyclerView.ViewHolder(tagItemView) {
         val tagButton: Button
         init {
             tagButton = tagItemView.findViewById(R.id.tagButton)
+            tagButton.setOnClickListener {
+                val tag = tagButton.text.toString()
+                if (selectedTag.contains(tag)) {
+                    tagButton.setBackgroundResource(R.drawable.tag_background)
+                    tagButton.setTextColor(ContextCompat.getColor(tagButton.context, R.color.gray))
+                    selectedTag.remove(tag)
+                } else {
+                    tagButton.setBackgroundResource(R.drawable.tag_background_selected)
+                    tagButton.setTextColor(ContextCompat.getColor(tagButton.context, R.color.white))
+                    selectedTag.add(tag)
+                }
+            }
         }
     }
 
