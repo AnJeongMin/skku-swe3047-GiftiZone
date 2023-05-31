5da412d4d235ddac34ae52db2e7aea6e7215edde
JeongMin <ajhappy12@gamil.com>
2023-06-01 01:07:27 +0900

    feat: 기프티콘 삭제 모달에 기프티콘 정보 추가

--- app/src/main/java/edu/skku/cs/giftizone/gifticonList/GifticonRemoveModal.kt
+++ app/src/main/java/edu/skku/cs/giftizone/gifticonList/GifticonRemoveModal.kt
@@ -3,6 +3,8 @@ package edu.skku.cs.giftizone.gifticonList
 import android.content.Context
 import android.view.LayoutInflater
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import edu.skku.cs.giftizone.R
 import edu.skku.cs.giftizone.dataClass.Gifticon
 
@@ -15,10 +17,17 @@ class GifticonRemoveModal(
     private val dialogLayout = inflater.inflate(R.layout.gifticon_remove_modal, null)
     private val cancelRemoveBtn = dialogLayout.findViewById<Button>(R.id.removeGifticonCancelButton)
     private val confirmRemoveBtn = dialogLayout.findViewById<Button>(R.id.removeGifticonConfirmButton)
+    private val removeGifticonImage = dialogLayout.findViewById<ImageView>(R.id.removeGifticonImage)
+    private val removeGifticonProvider = dialogLayout.findViewById<TextView>(R.id.removeGifticonProvider)
+    private val removeGifticonContent = dialogLayout.findViewById<TextView>(R.id.removeGifticonContent)
     private val removeGifticonDialog = androidx.appcompat.app.AlertDialog.Builder(context).create()
 
     init {
         removeGifticonDialog.setView(dialogLayout)
+//        removeGifticonImage.setImageBitmap(gifticon.image)
+        removeGifticonProvider.text = gifticon.provider
+        removeGifticonContent.text = gifticon.content
+
         confirmRemoveBtn.setOnClickListener {
             removeGifticonHandler()
             removeGifticonDialog.dismiss()
