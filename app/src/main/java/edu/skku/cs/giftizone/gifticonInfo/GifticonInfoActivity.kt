89fb3d3e892c44f9c81e9274e9babc917e01466f
JeongMin <ajhappy12@gamil.com>
2023-06-02 03:02:40 +0900

    feat: 기프티콘 공유 딜레이 기능 추가

--- app/src/main/java/edu/skku/cs/giftizone/gifticonInfo/GifticonInfoActivity.kt
+++ app/src/main/java/edu/skku/cs/giftizone/gifticonInfo/GifticonInfoActivity.kt
@@ -4,13 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import edu.skku.cs.giftizone.R
 import edu.skku.cs.giftizone.dataClass.Gifticon
 import edu.skku.cs.giftizone.gifticonMap.GifticonMapActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class GifticonInfoActivity : AppCompatActivity() {
     private var gifticon: Gifticon? = null
+    private val SHARE_DELAY = 60 // 60s
+    private var shareDelay = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gifticon_info)
@@ -39,8 +47,31 @@ class GifticonInfoActivity : AppCompatActivity() {
     private fun setupGifticonShareButton() {
         val gifticonShareButton = findViewById<ImageView>(R.id.gifticonInfoShareButton)
         gifticonShareButton.setOnClickListener {
+            if (!isShareDelay())
+                return@setOnClickListener
+
             val gifticonShareModal = GifticonShareModal(this, gifticon!!)
             gifticonShareModal.show()
         }
+    }
+
+    private fun toast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun isShareDelay(): Boolean {
+        if (shareDelay > 0) {
+            toast("공유는 ${shareDelay}초 후에 가능합니다.")
+            return false
+        }
+        shareDelay = SHARE_DELAY
+        val scope = CoroutineScope(Dispatchers.Default) // Replace with your own scope
+        scope.launch {
+            while (shareDelay > 0) {
+                delay(1000L) // Delay for 1 second
+                shareDelay -= 1
+            }
+        }
+        return true
     }
 }
--- app/src/main/java/edu/skku/cs/giftizone/gifticonInfo/GifticonShareModal.kt
+++ app/src/main/java/edu/skku/cs/giftizone/gifticonInfo/GifticonShareModal.kt
@@ -2,9 +2,21 @@ package edu.skku.cs.giftizone.gifticonInfo
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.widget.Toast
+import com.google.gson.Gson
+import com.google.gson.JsonObject
 import edu.skku.cs.giftizone.R
 import edu.skku.cs.giftizone.dataClass.Gifticon
 import edu.skku.cs.giftizone.gifticonList.TagMenuAdapter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.IOException
+import java.util.*
 
 class GifticonShareModal(
     private val context: Context,
@@ -12,13 +24,52 @@ class GifticonShareModal(
 ){
     private val inflater = LayoutInflater.from(context)
     private val dialogLayout = inflater.inflate(R.layout.gifticon_share_modal, null)
+    private val shareIdText = dialogLayout.findViewById<androidx.appcompat.widget.AppCompatTextView>(R.id.shareIdText)
     private val shareDialog = androidx.appcompat.app.AlertDialog.Builder(context).create()
 
     init {
         shareDialog.setView(dialogLayout)
+        shareIdText.text = getShareId()
+//        requestGifticonShare(gifticon, shareIdText.text.toString())
     }
 
     fun show() {
         shareDialog.show()
+    }
+
+    private fun getShareId(): String {
+        val random = Random(System.currentTimeMillis())
+        val randomNumber = random.nextInt(1000000)
+
+        return String.format("%06d", randomNumber)
+    }
+
+    private fun requestGifticonShare(gifticon: Gifticon, shareId: String) {
+        val client = OkHttpClient()
+        val url = "YOUR_POST_URL"
+
+        val jsonObject = JsonObject()
+        jsonObject.add("gifticon", Gson().toJsonTree(gifticon))
+        jsonObject.addProperty("shareId", shareId)
+        val requestBody = jsonObject.toString().toRequestBody("application/json; charset=utf-8".toMediaType())
+
+        val request = Request.Builder()
+            .url(url)
+            .post(requestBody)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                toast("공유가 정상적으로 되었습니다.")
+            }
+        })
+    }
+
+    private fun toast(message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
     }
 }
