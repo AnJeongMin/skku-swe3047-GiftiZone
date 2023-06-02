a5e1e1825fea14f96fa92858c56156a280ad3c9a
JeongMin <ajhappy12@gamil.com>
2023-06-02 23:41:24 +0900

    feat: tag repository 구현

--- app/src/main/java/edu/skku/cs/giftizone/common/TagRepository.kt
+++ app/src/main/java/edu/skku/cs/giftizone/common/TagRepository.kt
@@ -1,4 +1,42 @@
 package edu.skku.cs.giftizone.common
 
-class TagRepository {
+import android.content.ContentValues
+import android.content.Context
+
+class TagRepository(context: Context) {
+
+    private val dbManager = DatabaseManager(context)
+
+    fun saveTag(tag: String) {
+        val db = dbManager.writableDatabase
+
+        val values = ContentValues()
+        values.put(DatabaseManager.COLUMN_TAG_NAME, tag)
+
+        db.insert(DatabaseManager.TABLE_TAG, null, values)
+        db.close()
+    }
+
+    fun getAllTags(): ArrayList<String> {
+        val tags = ArrayList<String>()
+        val db = dbManager.readableDatabase
+        val cursor = db.rawQuery("SELECT * FROM ${DatabaseManager.TABLE_TAG}", null)
+
+        if (cursor.moveToFirst()) {
+            do {
+                val tag = cursor.getString(cursor.getColumnIndex(DatabaseManager.COLUMN_TAG_NAME))
+                tags.add(tag)
+            } while (cursor.moveToNext())
+        }
+        cursor.close()
+        db.close()
+        return tags
+    }
+
+    fun deleteTag(tag: String) {
+        val db = dbManager.writableDatabase
+        db.delete(DatabaseManager.TABLE_TAG, "${DatabaseManager.COLUMN_TAG_NAME} = ?", arrayOf(tag))
+        db.close()
+    }
+
 }
